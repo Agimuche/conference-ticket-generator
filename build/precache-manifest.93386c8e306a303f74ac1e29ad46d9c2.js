3c38e207654c525d3f773a310c5b4cbb7bdb47c8
agimuche1 <agimuche1@gmail.com>
2025-02-15 10:49:39 +0100

    Initial commit: Set up conference ticket generator project

---
+++ build/precache-manifest.93386c8e306a303f74ac1e29ad46d9c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831a4e583f6710e4ac76ddeb894d82f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30a3199a1c8ed13ba700",
+    "url": "/static/js/2.2a2ae8db.chunk.js"
+  },
+  {
+    "revision": "0d5908c820ccfae86c44",
+    "url": "/static/js/main.a4e17a47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/static/js/main.a4e17a47.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{13:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2);var o=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"Conference Ticket Generator"))};t.n(c).a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(o,null)),document.getElementById("root"))},3:function(e,n,t){e.exports=t(13)}},[[3,1,2]]]);
+//# sourceMappingURL=main.a4e17a47.chunk.js.map
